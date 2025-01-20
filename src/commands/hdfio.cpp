e6da20d5dd61f5b969232a3ac5c188c273089000
terencerl <terence.lobo@stfc.ac.uk>
2025-01-20 10:18:18 +0000

    log updates & rank sync

--- src/commands/hdfio.cpp
+++ src/commands/hdfio.cpp
@@ -56,8 +56,19 @@ void writeHdfTimelineCommand(TokenStream& tokens) {
     // if (mpi_rank == 0) {
     //     std::cout << "writing timeline to HDF5" << std::endl;
     // }
+
+    hsize_t dims[3] = {1, 1, (hsize_t)NOm};
+    hsize_t offset[3] = {(hsize_t)local_x_pos, (hsize_t)local_y_pos, 0};
+
     std::cout << "Rank "<<mpi_rank<<": writing timeline to HDF5" << std::endl;
+
+    // MPI_Barrier(MPI_COMM_WORLD);
+    // std::cout << "Rank "<<mpi_rank<<": All Ranks have reached writeHdfTimelineCommand "<< offset[0] << " " << offset[1] << " " << offset[2] << std::endl;
+
     writeHdf5TimelineDataset(data);
+
+    // MPI_Barrier(MPI_COMM_WORLD);
+    std::cout << "Rank "<<mpi_rank<<": All Ranks have executed writeHdfTimelineCommand "<< offset[0] << " " << offset[1] << " " << offset[2] << std::endl;
 }
 
 void readHdfTimelineCommand(TokenStream& tokens) {
@@ -74,7 +85,12 @@ void closeHdfCommand(TokenStream& tokens) {
     //     std::cout << "closing HDF file" << std::endl;
     // }
     std::cout <<"Rank "<<mpi_rank<<": closing HDF file" << std::endl;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    std::cout << "Rank "<<mpi_rank<<": All Ranks have reached closeHdfCommand" << std::endl;
+
     closeHdf5File();
+    std::cout << "Rank "<<mpi_rank<<": All Ranks have executed closeHdfCommand" << std::endl;
 }
 
 void closeHdfReadCommand(TokenStream& tokens) {
@@ -82,5 +98,8 @@ void closeHdfReadCommand(TokenStream& tokens) {
     //     std::cout << "closing HDF file for reading" << std::endl;
     // }
     std::cout <<"Rank "<<mpi_rank<<": closing HDF file for reading" << std::endl;
+    MPI_Barrier(MPI_COMM_WORLD);
+    std::cout <<"Rank "<<mpi_rank<<": closing HDF file for reading (after barrier)" << std::endl;
     closeHdf5ReadFile();
+    std::cout <<"Rank "<<mpi_rank<<": HDF file for reading has been closed" << std::endl;
 }
